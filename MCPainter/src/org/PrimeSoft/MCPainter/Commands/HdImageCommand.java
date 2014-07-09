a29a6c24adb0b94a7809cc2a8212464551d577ca
SB_Prime@poczta.onet.pl <SB_Prime@poczta.onet.pl>
2014-07-09 13:10:22 +0200

    MCPainter ADD: Added HD image command ADD: Added player rotation handling for HD image

--- MCPainter/src/org/PrimeSoft/MCPainter/Commands/HdImageCommand.java
+++ MCPainter/src/org/PrimeSoft/MCPainter/Commands/HdImageCommand.java
@@ -49,7 +49,7 @@ import org.bukkit.Material;
 import org.bukkit.Rotation;
 import org.bukkit.World;
 import org.bukkit.block.Block;
-import org.bukkit.entity.EntityType;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -68,7 +68,7 @@ public class HdImageCommand {
 
     public void Execute(MCPainterMain sender, Player player, IWorldEdit worldEdit, String[] args) {
         if (args.length != 2) {
-//            Help.ShowHelp(player, Commands.COMMAND_IMAGEHD);
+            Help.ShowHelp(player, Commands.COMMAND_IMAGEHD);
             return;
         }
 
@@ -88,33 +88,41 @@ public class HdImageCommand {
         private final Location m_location;
         private final BufferedImage m_img;
         private final MapHelper m_mapHelper;
+        private final BlockFace m_rotation;
 
-        private DrawMapCommand(Location location, int offsetX, int offsetY, BufferedImage img,
+        private DrawMapCommand(Location location, BlockFace face,
+                int offsetX, int offsetY, BufferedImage img,
                 MapHelper mapHelper) {
             m_location = location;
-            
+
             int x = Math.min(offsetX + 127, img.getWidth() - 1);
-                int y = Math.min(offsetY + 127, img.getHeight() - 1);
-            
+            int y = Math.min(offsetY + 127, img.getHeight() - 1);
+
             m_img = CropFilter.crop(img, offsetX, offsetY, x, y, false);
             m_mapHelper = mapHelper;
+            m_rotation = face;
         }
 
         @Override
         public void execute(BlockPlacer blockPlacer, BlockLoger loger) {
             Chunk chunk = m_location.getChunk();
             if (!chunk.isLoaded()) {
-                if (!chunk.load()){
+                if (!chunk.load()) {
                     return;
                 }
             }
-            
+
             World w = m_location.getWorld();
-            Block block = w.getBlockAt(m_location);            
-            block.setType(Material.STONE);
-            ItemFrame frame =  (ItemFrame)w.spawnEntity(m_location, EntityType.ITEM_FRAME);
-            frame.setRotation(Rotation.NONE);                        
-                        
+            Block block = w.getBlockAt(m_location);
+            if (!isSolid(block.getType())) {
+                block.setType(Material.STONE);
+            }
+
+            ItemFrame frame = w.spawn(m_location, ItemFrame.class);
+            frame.teleport(block.getRelative(m_rotation).getLocation());
+            frame.setFacingDirection(m_rotation, true);
+            frame.setRotation(Rotation.NONE);
+
             MapView mapView = Bukkit.createMap(w);
             m_mapHelper.storeMap(mapView, m_img);
             m_mapHelper.drawImage(mapView, m_img);
@@ -125,6 +133,32 @@ public class HdImageCommand {
         @Override
         public Location getLocation() {
             return m_location;
+        }
+
+        private boolean isSolid(Material m) {
+            if (m == Material.AIR) {
+                return false;
+            }
+            if (m == Material.LAVA) {
+                return false;
+            }
+            if (m == Material.STATIONARY_LAVA) {
+                return false;
+            }
+            if (m == Material.WATER) {
+                return false;
+            }
+            if (m == Material.STATIONARY_WATER) {
+                return false;
+            }
+            if (m == Material.ENDER_PORTAL) {
+                return false;
+            }
+            if (m == Material.PORTAL) {
+                return false;
+            }
+
+            return m.isSolid() && m.isBlock();
         }
     }
 
@@ -137,6 +171,7 @@ public class HdImageCommand {
         private final HdImageCommand m_this;
         private final IEditSession m_session;
         private final ILocalSession m_lSession;
+        private final BlockFace m_rotation;
 
         private CommandThread(HdImageCommand command, MCPainterMain sender, Player player,
                 String url, IWorldEdit worldEdit, ICuboidSelection selection) {
@@ -145,15 +180,16 @@ public class HdImageCommand {
             m_player = player;
             m_url = url;
             m_selection = selection;
-            m_lSession = worldEdit.getSession(player);
             ILocalPlayer localPlayer = worldEdit.wrapPlayer(player);
+            m_rotation = calcHeading(localPlayer.getYaw());
+            m_lSession = worldEdit.getSession(player);
             m_session = m_lSession.createEditSession(localPlayer);
         }
 
         @Override
         public void run() {
             FilterManager fm = FilterManager.getFilterManager(m_player);
-            double price = ConfigProvider.getCommandPrice("image") + fm.getPrice();
+            double price = ConfigProvider.getCommandPrice("imagehd") + fm.getPrice();
             synchronized (FoundManager.getMutex()) {
                 if (price > 0 && FoundManager.getMoney(m_player) < price) {
                     MCPainterMain.say(m_player, ChatColor.RED + "You don't have sufficient funds to apply all the filters and draw the map.");
@@ -169,10 +205,10 @@ public class HdImageCommand {
 
                 img = fm.applyFilters(img, null);
                 final BufferedImage fImg = img;
-                int hh = img.getHeight();
-                int ww = img.getWidth();
+                int imgH = img.getHeight();
+                int imgW = img.getWidth();
 
-                if (!PermissionManager.checkImage(m_player, ww, hh)) {
+                if (!PermissionManager.checkImage(m_player, imgW, imgH)) {
                     return;
                 }
 
@@ -182,6 +218,7 @@ public class HdImageCommand {
                 int w = m_selection.getWidth();
                 int h = m_selection.getHeight();
                 int kx, kz;
+
                 if (w > 1 && l > 1) {
                     MCPainterMain.say(m_player, ChatColor.RED + "Invalid selection area.");
                     return;
@@ -193,21 +230,28 @@ public class HdImageCommand {
                     kz = 1;
                 }
 
-                int bHeight = hh / 128 + (hh % 128 != 0 ? 1 : 0);
-                int bWidth = ww / 128 + (ww % 128 != 0 ? 1 : 0);
+                int bHeight = imgH / 128 + (imgH % 128 != 0 ? 1 : 0);
+                int bWidth = imgW / 128 + (imgW % 128 != 0 ? 1 : 0);
                 if (h < bHeight || (w < bWidth && l < bWidth)) {
                     MCPainterMain.say(m_player, ChatColor.RED + "The selection is to smal, required: " + bWidth + "x" + bHeight);
                     return;
                 }
 
                 Location pos = new Location(minPoint.getWorld(), minPoint.getBlockX(), maxPoint.getBlockY(), minPoint.getBlockZ());
+                if (m_rotation == BlockFace.NORTH
+                        || m_rotation == BlockFace.EAST) {
+                    pos = pos.add(kx * (bWidth - 1), 0, kz * (bWidth - 1));
+                    kx *= -1;
+                    kz *= -1;
+                }
                 MCPainterMain.say(m_player, "Drawing image...");
                 BlockLoger loger = new BlockLoger(m_player, m_lSession, m_session, m_sender);
 
                 for (int py = 0; py < bHeight; py++) {
                     Location tmp = pos.clone();
                     for (int px = 0; px < bWidth; px++) {
-                        loger.logCommand(new DrawMapCommand(tmp.clone(), px * 128, py * 128, fImg, m_mapHelper));
+                        loger.logCommand(new DrawMapCommand(tmp.clone(), m_rotation,
+                                px * 128, py * 128, fImg, m_mapHelper));
                         tmp = tmp.add(kx, 0, kz);
                     }
                     pos = pos.add(0, -1, 0);
@@ -216,8 +260,23 @@ public class HdImageCommand {
                 loger.logMessage("Drawing image done.");
                 loger.logEndSession();
                 loger.flush();
-                
+
                 FoundManager.subtractMoney(m_player, price);
+            }
+        }
+
+        private BlockFace calcHeading(double yaw) {
+            yaw = (yaw + 360) % 360;
+            if (yaw < 45) {
+                return BlockFace.NORTH;
+            } else if (yaw < 135) {
+                return BlockFace.EAST;
+            } else if (yaw < 225) {
+                return BlockFace.SOUTH;
+            } else if (yaw < 315) {
+                return BlockFace.WEST;
+            } else {
+                return BlockFace.NORTH;
             }
         }
     }
